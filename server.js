68325fd3bd244df2176741f4402a1fb8a2b34cc0
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2022-07-18 15:35:08 +0100

    swagger documentation

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDoc = yaml.load('./swagger.yaml')
 const PORT = process.env.PORT || 3000
 app.use(express.json())
+app.use(cors())
+
+
 
 require('dotenv').config()
 const connectDB = require('./db/connect')
@@ -10,10 +17,10 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
 const postRouter = require('./routes/post')
 const authorization = require('./middleware/authorization')
-
 app.get('/',(req,res)=>{
-    res.send('simple stutern assignment')
+    res.send('<h1>post API</h1><a href="/api-docs">Swagger Documentation</a>')
 })
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/post',authorization,postRouter)
 
