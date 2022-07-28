9492d963a495a73610adb8db4c0115d09d2beab0
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2022-07-28 11:07:52 +0100

    heroku notified

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const authController = require('../controller/auth')
-const authorization = require('../middleware/authorization')
 
-router.post('/login',authorization,authController.login)
+router.post('/login',authController.login)
 router.post('/register',authController.register)
 
 
